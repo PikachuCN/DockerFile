80d67cf2eed23c8d5e89b2c546db97198fd5c9d2
PikachuCN <47570641+PikachuCN@users.noreply.github.com>
2021-11-30 01:21:41 +0800

    Update Xray_QUIC.sh

--- Xray_QUIC.sh
+++ Xray_QUIC.sh
@@ -63,18 +63,6 @@ cat > /usr/local/etc/xray/config.json<<-EOF
 }
 EOF
 
- remove_bbr_lotserver
- echo "net.core.default_qdisc=fq" >>/etc/sysctl.d/99-sysctl.conf
- echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.d/99-sysctl.conf
- sysctl --system
-echo -e "${Info}BBR+FQ修改成功，重启生效！"
-systemctl enable xray
-systemctl restart xray
-
-
-
-#卸载bbr+锐速
-remove_bbr_lotserver() {
   sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.d/99-sysctl.conf
   sed -i '/net.core.default_qdisc/d' /etc/sysctl.d/99-sysctl.conf
   sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.d/99-sysctl.conf
@@ -91,4 +79,14 @@ remove_bbr_lotserver() {
   clear
   # echo -e "${Info}:清除bbr/lotserver加速完成。"
   # sleep 1s
-}
+
+ echo "net.core.default_qdisc=fq" >>/etc/sysctl.d/99-sysctl.conf
+ echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.d/99-sysctl.conf
+ sysctl --system
+echo -e "${Info}BBR+FQ修改成功，重启生效！"
+systemctl enable xray
+systemctl restart xray
+echo -e "安装完成！"
+echo "正在获取 IP 地址信息，请耐心等待"
+local_ip=$(curl -4L api64.ipify.org)
+echo -e "vless://c6bdab75-75be-446a-84f9-2fee409b725d@${local_ip}:3389?encryption=none&security=none&type=quic&headerType=none&quicSecurity=&key=#12.qspp.net"
